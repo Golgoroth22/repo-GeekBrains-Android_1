1fd560c32cad5a2ba277d542dcaf2af009337771
Golgoroth22 <falinv22@gmail.com>
2018-01-03 22:57:46 +0700

    Update sand_msg_layout. Update textView to react on selected variant in spiner

--- Main_Project/TaskTwo/app/src/main/java/ru/valentin/falinv22/tasktwo/SendMessageActivity.java
+++ Main_Project/TaskTwo/app/src/main/java/ru/valentin/falinv22/tasktwo/SendMessageActivity.java
@@ -2,12 +2,37 @@ package ru.valentin.falinv22.tasktwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SendMessageActivity extends AppCompatActivity {
+    private Spinner selectReceiver;
+    private TextView receiverNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
+
+        initUI();
+    }
+
+    private void initUI() {
+        receiverNumber = findViewById(R.id.send_message_view_receiver_number);
+        selectReceiver = findViewById(R.id.send_message_view_select);
+        selectReceiver.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String[] choose = getResources().getStringArray(R.array.receivers_list_number);
+                receiverNumber.setText(choose[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
