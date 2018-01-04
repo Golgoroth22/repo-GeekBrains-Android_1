6440a38ae25fb7f40f5eb7fd066d49c56b19554b
Golgoroth22 <falinv22@gmail.com>
2018-01-04 21:30:18 +0700

    Add Intent.ACTION_SEND to send button

--- Main_Project/TaskTwo/app/src/main/java/ru/valentin/falinv22/tasktwo/SendMessageActivity.java
+++ Main_Project/TaskTwo/app/src/main/java/ru/valentin/falinv22/tasktwo/SendMessageActivity.java
@@ -1,15 +1,21 @@
 package ru.valentin.falinv22.tasktwo;
 
+import android.Manifest;
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class SendMessageActivity extends AppCompatActivity {
     private Spinner selectReceiver;
     private TextView receiverNumber;
+    private Button sendMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +40,28 @@ public class SendMessageActivity extends AppCompatActivity {
 
             }
         });
+
+        sendMessage = findViewById(R.id.send_msq_view_button);
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMsq(view);
+            }
+        });
+    }
+
+    public void sendMsq(View view) {
+        String sms = sendMessage.getText().toString();
+//        String number = receiverNumber.getText().toString();
+//        SmsManager smsManager = SmsManager.getDefault();
+//        smsManager.sendTextMessage(number, null, sms, null, null);
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, sms);
+        sendIntent.setType("text/plain");
+
+        if (sendIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(sendIntent);
+        }
     }
 }
