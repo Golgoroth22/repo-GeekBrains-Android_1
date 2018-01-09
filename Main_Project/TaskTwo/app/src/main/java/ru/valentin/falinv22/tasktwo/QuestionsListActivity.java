81b5a1a1b6503ab6f92c1796f5a220fe956a9bb1
Valentin <falinv22@gmail.com>
2018-01-09 21:46:02 +0700

    Update add new question button view

--- Main_Project/TaskTwo/app/src/main/java/ru/valentin/falinv22/tasktwo/QuestionsListActivity.java
+++ Main_Project/TaskTwo/app/src/main/java/ru/valentin/falinv22/tasktwo/QuestionsListActivity.java
@@ -30,6 +30,7 @@ public class QuestionsListActivity extends AppCompatActivity {
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setRippleColor(1);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
