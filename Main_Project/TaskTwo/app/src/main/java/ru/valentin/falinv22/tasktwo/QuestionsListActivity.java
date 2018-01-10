adfe2f51af096a89344f54a5bde6c9cd2665d280
Valentin <falinv22@gmail.com>
2018-01-10 19:39:59 +0700

    Update QuestionListActivity

--- Main_Project/TaskTwo/app/src/main/java/ru/valentin/falinv22/tasktwo/QuestionsListActivity.java
+++ Main_Project/TaskTwo/app/src/main/java/ru/valentin/falinv22/tasktwo/QuestionsListActivity.java
@@ -9,13 +9,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import ru.valentin.falinv22.tasktwo.data.FakeDB;
 import ru.valentin.falinv22.tasktwo.quiz.Question;
 
 public class QuestionsListActivity extends AppCompatActivity {
+    public static final String ANSWER_PREF = "Ответ - ";
+    public static final String QUESTION = "question";
+    public static final String ANSWER = "answer";
+    public static final String YES = "ДА";
+    public static final String NO = "НЕТ";
     private ListView questionListView;
     private ArrayList<Question> db;
 
@@ -23,19 +30,14 @@ public class QuestionsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         db = new ArrayList<>();
         db.addAll(FakeDB.getInstance(getApplicationContext()).getQuestionList());
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
                 Intent intent = new Intent(getApplicationContext(), AddNewQuestionActivity.class);
                 startActivity(intent);
             }
@@ -46,12 +48,24 @@ public class QuestionsListActivity extends AppCompatActivity {
 
     private void initUI() {
         questionListView = findViewById(R.id.questions_list);
-        ArrayList<String> questionsList = new ArrayList<>();
-        for (Question q : db) {
-            questionsList.add(q.getQuestion());
+
+        ArrayList<HashMap<String, String>> dbList = new ArrayList<>();
+        HashMap<String, String> map;
+
+        for (int i = 0; i < FakeDB.getInstance(getApplicationContext()).getQuestionList().size(); i++) {
+            map = new HashMap<>();
+            String q = FakeDB.getInstance(getApplicationContext()).getQuestionList().get(i).getQuestion();
+            boolean a = FakeDB.getInstance(getApplicationContext()).getQuestionList().get(i).getAnswer();
+            map.put(QUESTION, q);
+            if (a) map.put(ANSWER, ANSWER_PREF + YES);
+            if (!a) map.put(ANSWER, ANSWER_PREF + NO);
+            dbList.add(map);
         }
-        ArrayAdapter<String> questionsAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, questionsList);
-        questionListView.setAdapter(questionsAdapter);
+
+        SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(), dbList, android.R.layout.simple_list_item_2,
+                new String[]{QUESTION, ANSWER},
+                new int[]{android.R.id.text1, android.R.id.text2});
+        questionListView.setAdapter(adapter);
     }
 
 }
