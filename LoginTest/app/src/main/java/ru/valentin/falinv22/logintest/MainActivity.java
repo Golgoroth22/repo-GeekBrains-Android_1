c82dd79d5c1ad932409b5161f75b5e10f5bc1d01
Valentin <falinv22@gmail.com>
2018-01-22 20:34:42 +0700

    Delete old projects and start new

--- LoginTest/app/src/main/java/ru/valentin/falinv22/logintest/MainActivity.java
+++ LoginTest/app/src/main/java/ru/valentin/falinv22/logintest/MainActivity.java
@@ -1,6 +1,5 @@
 package ru.valentin.falinv22.logintest;
 
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,10 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        LoginFragment loginFragment = new LoginFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.login_fragment_container, loginFragment);
-        transaction.commit();
     }
 }
